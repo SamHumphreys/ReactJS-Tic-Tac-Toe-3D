287793f20f427580c40fa0e7f86fe67f44d65f32
Sam Humphreys <s-humphreys@hotmail.com>
2017-05-11 22:31:45 +1000

    responsive updated

--- src/components/status.js
+++ src/components/status.js
@@ -6,14 +6,14 @@ const Status = (props) => {
       return (
         <div className='status main'>
           <p>The game is a draw :(</p>
-          <p>Click anywhere to play again!</p>
+          <p className='reset'>Click anywhere to play again!</p>
         </div>
       )
     } else {
       return (
         <div className='status main'>
           <p>{props.winner} won the game!</p>
-          <p>Click anywhere to play again!</p>
+          <p className='reset'>Click anywhere to play again!</p>
         </div>
       )
     }
